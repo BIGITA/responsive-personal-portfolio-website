9fbb2039b49a4b078a7d19c26ff13bd4f0dd5db1
ibrahim kenia <ibrahimkenia2@gmail.com>
2022-08-27 03:01:16 +0100

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Nav from './components/nav/Nav'
+import Portfolio from './components/portfolio/Portolio'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+import About from './components/about/About'
+
+export const App = () => {
+  return (
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+
+
+    
+    </>
+  )
+}
+export default App;
---
+++ src/components/portfolio/Portolio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './portfolio.css'
+function Portolio() {
+  return (
+    <div>Portolio</div>
+  )
+}
+
+export default Portolio
